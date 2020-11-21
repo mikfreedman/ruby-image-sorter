95c0819bc13d29c924bd3c1c1f68ffc477ae1f1b
Mik Freedman <github@michael-freedman.com>
2020-11-21 12:18:09 -0500

    initial bundle gem

---
+++ lib/ruby/image/sorter.rb
@@ -0,0 +1,10 @@
+require "ruby/image/sorter/version"
+
+module Ruby
+  module Image
+    module Sorter
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/ruby/image/sorter/version.rb
@@ -0,0 +1,7 @@
+module Ruby
+  module Image
+    module Sorter
+      VERSION = "0.1.0"
+    end
+  end
+end
